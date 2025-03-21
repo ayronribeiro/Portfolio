fbd2f0e9772486e493446800b24efc5607f041ff
ayronribeiro <ayronribeiro.r@outlook.com>
2025-03-21 15:21:44 -0300

    meta changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,19 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="scroll-smooth" suppressHydrationWarning>
       <head>
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="apple-touch-icon" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="/favicon.png"/>
+        <link rel="apple-touch-icon" href="/favicon.png"/>
+
+        <meta property="og:title" content="Ayron Rivero | Software Developer" />
+        <meta property="og:description" content="Software Developer com mais de 5 anos de experiência, especializado em WordPress, Angular, React, Next.js e Strapi."/>
+        <meta property="og:image" content="/thumb.png"/> 
+        <meta property="og:url" content="https://ayronrivero.vercel.app/"/> 
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Ayron Rivero"/>
+        <meta name="twitter:title" content="Ayron Rivero | Software Developer"/>
+        <meta name="twitter:description" content="Software Developer com mais de 5 anos de experiência, especializado em WordPress, Angular, React, Next.js e Strapi."/>
+        <meta name="twitter:image" content="/thumb.png"/>
+        <meta name="twitter:card" content="summary_large_image"/>
 
         <script
           dangerouslySetInnerHTML={{
